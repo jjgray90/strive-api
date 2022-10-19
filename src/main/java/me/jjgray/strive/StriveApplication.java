3d27d6b3e942d108ba806082fd2bbe842e70b819
Your Name <jjgray90@gmail.com>
2022-10-19 15:07:54 +0100

    exclude DataSourceConfig

--- src/main/java/me/jjgray/strive/StriveApplication.java
+++ src/main/java/me/jjgray/strive/StriveApplication.java
@@ -2,8 +2,9 @@ package me.jjgray.strive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class StriveApplication {
 
 	public static void main(String[] args) {
